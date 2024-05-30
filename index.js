661f06bfcbaa127d885b09fc5f5c6f0f6de9fb39
Aditya-04030 <aditya123sahniaz@gmail.com>
2024-05-30 20:13:41 +0530

    2.0.0

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express=require('express');
 const bodyparser=require('body-parser');
+const path=require('path');
 const db=require('./connectDB');
 const PORT=process.env.PORT||3000;
 const urlRoutes=require('./routes/url')
+const staticRoutes=require('./routes/staticRoutes')
 const app=express();
 app.use(bodyparser.json());
-app.use('/',urlRoutes)
-
+app.use(bodyparser.urlencoded())
+app.set('view engine','ejs')
+app.set('views',path.resolve('./views'));
+app.use('/',staticRoutes);
+app.use('/URL',urlRoutes);
 
 
 
--- routes/url.js
+++ routes/url.js
@@ -4,17 +4,17 @@ const URL=require('./../models/urlSchema')
 const router=express.Router();
 
 
-router.post('/URL', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const userurl= req.body.url; 
-        console.log(userurl);
         if (!userurl) {
             return res.status(400).json({ message: 'PLEASE ENTER A URL' });
         }
         const id = shortID.generate();
         const shorturl = new URL({ shortID: id, redirectURL:userurl});
         const response = await shorturl.save();
-        res.status(201).json(response.shortID); 
+        res.render('home',{id:response.shortID})
+        // res.status(201).json(response.shortID); 
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'INTERNAL SERVER ERROR', error });
